a9e272e1f447c690e4175a3ed682a1a45ba3b49f
Chris Constantine <cconstan@gmail.com>
2018-08-01 23:28:08 -0700

    disable laser

--- pixlib/src/face_finder.cpp
+++ pixlib/src/face_finder.cpp
@@ -164,6 +164,16 @@ namespace Pixlib {
       // config.enable_stream(RS2_STREAM_DEPTH, 1);
 
       pipeline_profile = pipe->start(config);
+
+      rs2::device selected_device = pipeline_profile.get_device();
+      auto depth_sensor = selected_device.first<rs2::depth_sensor>();
+
+
+      if (depth_sensor.supports(RS2_OPTION_EMITTER_ENABLED))
+      {
+          depth_sensor.set_option(RS2_OPTION_EMITTER_ENABLED, 0.f); // Disable emitter
+      }
+
       started = true;
     } else if (started && device_count == 0) {
       pipe->stop();
