d4cd468642d7fb896210bf936fdd80c9e54340af
Chris Constantine <cconstan@gmail.com>
2018-08-11 12:21:10 -0700

    log reconnects to opc servers

--- pixlib/src/fade_candy.cpp
+++ pixlib/src/fade_candy.cpp
@@ -6,7 +6,7 @@ namespace Pixlib {
   FadeCandy::FadeCandy(const std::string& hostname, size_t size)
    : hostname(hostname), size(size),running(true), writer_thread(&FadeCandy::thread_method, this)
   {
-    ALOGV("connecting to: %s\n", hostname.c_str());
+    // ALOGV("connecting to: %s\n", hostname.c_str());
     //opc_client.resolve(hostname.c_str());
 
     int frameBytes = size * 3;
@@ -51,6 +51,10 @@ namespace Pixlib {
 
       // fprintf(stderr, "%s: thread (%d)!\n", hostname.c_str(), opc_client.isConnected());
       if(running || opc_client.isConnected()) {
+
+        if (!opc_client.isConnected()) {
+            fprintf(stderr, "Connecting to: %s\n", hostname.c_str());
+        }
         opc_client.write(hostname.c_str(), framebuffer);
       }
     }
